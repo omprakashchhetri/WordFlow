98657731835f642712bf0fe14cd04d9be51bc5f4
omprakashchhetri <omprakashchhetri>
2023-07-02 00:40:49 +0530

    version 3

--- api/index.js
+++ api/index.js
@@ -64,35 +64,15 @@ app.post("/login", async (req, res) => {
   }
 });
 
-// app.post("/login", async (req, res) => {
-//   const { username, password } = req.body;
-//   const userDoc = await User.findOne({ username });
-//   const passOk = bcrypt.compareSync(password, userDoc.password);
-//   if (passOk) {
-//     // loggedin
-//     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-//       if (err) throw err;
-//       res.cookie("token", token).json({
-//         id: userDoc._id,
-//         username,
-//       });
-//     });
-//   } else {
-//     res.status(400).json("wrong credentials");
-//   }
-// });
-
-// profile page route
-// ...
-
-// profile page route
+// profile page routej
 app.get("/profile", (req, res) => {
   const { token } = req.cookies;
   if (token) {
     jwt.verify(token, secret, {}, (err, info) => {
       if (err) {
-        // Handle JWT verification error
+        // to handle JWT verification error
         res.status(401).json({ error: "Invalid token" });
+        console.error("JWT verification error:", err);
       } else {
         // Token is valid, proceed with the profile endpoint logic
         res.json(info);
@@ -104,37 +84,43 @@ app.get("/profile", (req, res) => {
   }
 });
 
-// ...
-
-// app.get("/profile", (req, res) => {
-//   const { token } = req.cookies;
-//   jwt.verify(token, secret, {}, (err, info) => {
-//     if (err) throw err;
-//     res.json(info);
-//   });
-// });
-
 // logout page route
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json("ok");
 });
 
-// post route for before creating a post
+const allowedImageTypes = [
+  "image/jpeg",
+  "image/jpg",
+  "image/webp",
+  "image/png",
+  "image/gif",
+]; // Specify the allowed image mimetypes
+
 app.post("/post", uploadMiddleware.single("file"), async (req, res, next) => {
   try {
     if (!req.file) {
       return res.status(400).json({ error: "No file found" });
     }
 
-    const { originalname, path } = req.file;
+    const { originalname, path, mimetype } = req.file;
     const parts = originalname.split(".");
     const ext = parts[parts.length - 1];
     const newPath = path + "." + ext;
     fs.renameSync(path, newPath);
 
+    if (!allowedImageTypes.includes(mimetype)) {
+      // Checking if the uploaded file's mimetype is allowed
+      fs.unlinkSync(newPath); // Delete the file
+      return res
+        .status(400)
+        .json({ error: "Invalid file type. Only image files are allowed." });
+    }
+
     const { token } = req.cookies;
     jwt.verify(token, secret, {}, async (err, info) => {
       if (err) {
+        fs.unlinkSync(newPath); // Delete the file
         return res.status(401).json({ error: "Unauthorized" });
       }
 
@@ -151,45 +137,69 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res, next) => {
     });
   } catch (err) {
     next(err); // Passing the error to the error-handling middleware
+    console.error("Error processing file:", error);
+
+    // Cleanup uploaded file if it exists
+    if (req.file && req.file.path) {
+      fs.unlinkSync(req.file.path);
+    }
+
+    res.status(500).json({ error: "File upload failed." });
   }
 });
 
 app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
-  let newPath = null;
-  if (req.file) {
-    const { originalname, path } = req.file;
-    const parts = originalname.split(".");
-    const ext = parts[parts.length - 1];
-    newPath = path + "." + ext;
-    fs.renameSync(path, newPath);
-  }
-  const { token } = req.cookies;
-  jwt.verify(token, secret, {}, async (err, info) => {
-    if (err) throw err;
-    const { id, title, summary, content } = req.body;
-    const postDoc = await Post.findById(id);
-    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-    if (!isAuthor) {
-      return res.status(400).json("you are not the author");
-    }
-    await postDoc.updateOne({
-      title,
-      summary,
-      content,
-      cover: newPath ? newPath : postDoc.cover,
-    });
+  try {
+    let newPath = null;
+    if (req.file) {
+      const { originalname, path, mimetype } = req.file;
+      const parts = originalname.split(".");
+      const ext = parts[parts.length - 1];
+      newPath = path + "." + ext;
 
-    res.json(postDoc);
-  });
+      if (!allowedImageTypes.includes(mimetype)) {
+        // Checking if the uploaded file's mimetype is allowed
+        fs.unlinkSync(path); // Delete the file
+        return res
+          .status(400)
+          .json({ error: "Invalid file type. Only image files are allowed." });
+      }
+
+      fs.renameSync(path, newPath);
+    }
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+      if (err) throw err;
+      const { id, title, summary, content } = req.body;
+      const postDoc = await Post.findById(id);
+      const isAuthor =
+        JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+      if (!isAuthor) {
+        if (newPath) {
+          fs.unlinkSync(newPath); // Deleteing the file if it was uploaded
+        }
+        return res.status(400).json("You are not the author");
+      }
+      await postDoc.updateOne({
+        title,
+        summary,
+        content,
+        cover: newPath ? newPath : postDoc.cover,
+      });
+
+      res.json(postDoc);
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // post route for after creating a post
 app.get("/post", async (req, res) => {
   res.json(
-    await Post.find()
-      .populate("author", ["username"])
-      .sort({ createdAt: -1 })
-      .limit(20)
+    await Post.find().populate("author", ["username"]).sort({ createdAt: -1 })
+    // .limit(5)
   );
 });
 
